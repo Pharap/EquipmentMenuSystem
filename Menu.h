c531f6ade20dfe70d57f6a888d4bd1b8bdaa8d49
Pharap <pharap@hotmail.com>
2017-08-28 11:11:57 +0100

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -3,35 +3,51 @@
 #include <Arduboy2.h>
 #include "Utility.h"
 
-using MenuAction = void (*)(Player *);
+using MenuAction = void (*)(Player &);
 
 struct MenuOption
 {
-  const char * Text;
-  MenuAction Action;
+	const char * Text;
+	MenuAction Action;
 };
 
 struct Menu
 {
-  const char * Title;
-  const MenuOption * Options;
-  uint8_t OptionsLength;
+	const char * Title;
+	const MenuOption * Options;
+	uint8_t OptionsLength;
 };
 
 void DrawMenu(Arduboy2 & arduboy, const Menu & menu, const uint8_t & selected)
 {
-  arduboy.println(AsFlashStringHelper(menu.Title));
-  
-  for (uint8_t i = 0; i < menu.OptionsLength; ++i)
-  {
-	arduboy.print((i == selected) ? F("> ") : F("  "));
-    arduboy.println(AsFlashStringHelper(pgm_read_word(&menu.Options[i])));
-  }
+	arduboy.println(AsFlashStringHelper(menu.Title));
+
+	for (uint8_t i = 0; i < menu.OptionsLength; ++i)
+	{
+		arduboy.print((i == selected) ? F("> ") : F("  "));
+		arduboy.println(AsFlashStringHelper(pgm_read_word(&menu.Options[i].Text)));
+	}
 }
 
 inline void DrawProgmemMenu(Arduboy2 & arduboy, const Menu & progmemMenu, const uint8_t & selected)
 {
-  Menu menu;
-  memcpy_P(&menu, &progmemMenu, sizeof(Menu));
-  DrawMenu(arduboy, menu, selected);
+	Menu menu;
+	memcpy_P(&menu, &progmemMenu, sizeof(Menu));
+	DrawMenu(arduboy, menu, selected);
+}
+
+bool ApplyMenuAction(Player & player, const Menu & menu, const uint8_t & selected)
+{
+	if(selected > menu.OptionsLength)
+		return false;
+	MenuAction action = reinterpret_cast<MenuAction>(pgm_read_word(&menu.Options[selected].Action));
+	action(player);
+	return true;
+}
+
+inline bool ApplyProgmemMenuAction(Player & player, const Menu & progmemMenu, const uint8_t & selected)
+{
+	Menu menu;
+	memcpy_P(&menu, &progmemMenu, sizeof(Menu));
+	return ApplyMenuAction(player, menu, selected);
 }
